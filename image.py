c25332849d23fe74d74832a4ca8809856fe4644e
jollicliffe <47151022+jollicliffe@users.noreply.github.com>
2020-06-30 03:09:32 +0100

    first upload

---
+++ image.py
@@ -0,0 +1,39 @@
+import PIL
+import binascii
+import codecs
+
+
+def conToBytes(filename):
+    with open(filename, "rb") as f:
+        content = f.read()
+    image_bytes = content
+    return image_bytes
+
+def conToHex(bytes):
+    return bytes.hex()
+
+def conToBase64(hex):
+    return codecs.encode(codecs.decode(hex, 'hex'), 'base64').decode()
+
+#takes hex value please
+def writeImage(data):
+    try:
+        new=input("enter new filename: ")
+        with open(new , "wb") as f:
+            f.write(binascii.unhexlify(data))
+        print("created new image file.")
+    except:
+        print("Unable to print new image")
+
+def main():
+    filename = 'image.jpg'
+    with open(filename, "rb") as f:
+        data=f.read()
+    hex = conToHex(data)
+    writeImage(hex)
+
+
+if __name__ == "__main__":
+    print("please import")
+else:
+    main()
